ce7ddd1d38cf8300064c87d57b92f33c351028fe
Kedwin Araya <karaya@marandino.dev>
2024-07-21 16:31:03 -0600

    adds: Bootstrap validation for forms

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
